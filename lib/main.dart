2ffcd5afff55edfeafe6ff363ae222ea1c190fac
xshxr <iamashar17@gmail.com>
2024-07-20 14:37:33 +0500

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
-// ignore_for_file: library_private_types_in_public_api
-
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/todo_list_screen.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
 
 void main() {
+  tz.initializeTimeZones();
   runApp(const MyApp());
 }
 
@@ -16,7 +17,18 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   bool _isDarkMode = false;
-  TimeOfDay _selectedTime = const TimeOfDay(hour: 0, minute: 0);
+  final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  @override
+  void initState() {
+    super.initState();
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
+    const InitializationSettings initializationSettings =
+        InitializationSettings(android: initializationSettingsAndroid);
+    _flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  }
 
   void _toggleTheme() {
     setState(() {
@@ -24,37 +36,35 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
-  Future<void> _selectAlarmTime(BuildContext context) async {
-    final TimeOfDay? pickedTime = await showTimePicker(
-      context: context,
-      initialTime: _selectedTime,
-    );
-    if (pickedTime != null && pickedTime != _selectedTime) {
-      setState(() {
-        _selectedTime = pickedTime;
-      });
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'To-Do List',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
         brightness: Brightness.light,
-        scaffoldBackgroundColor: Colors.white,
+        scaffoldBackgroundColor: Colors.grey.shade200,
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(color: Colors.black87),
+          bodyMedium: TextStyle(color: Colors.black87),
+          displayLarge: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+        ),
       ),
       darkTheme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
         brightness: Brightness.dark,
-        scaffoldBackgroundColor: const Color.fromARGB(255, 18, 18, 18),
+        scaffoldBackgroundColor: const Color(0xFF212121),
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(color: Colors.white70),
+          bodyMedium: TextStyle(color: Colors.white70),
+          displayLarge: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+        ),
       ),
       themeMode: _isDarkMode ? ThemeMode.dark : ThemeMode.light,
       home: ToDoListScreen(
         isDarkMode: _isDarkMode,
         toggleTheme: _toggleTheme,
-        selectAlarmTime: _selectAlarmTime, // Pass selectAlarmTime callback
+        flutterLocalNotificationsPlugin: _flutterLocalNotificationsPlugin,
       ),
     );
   }
@@ -63,12 +73,12 @@ class _MyAppState extends State<MyApp> {
 class ToDoListScreen extends StatefulWidget {
   final bool isDarkMode;
   final VoidCallback toggleTheme;
-  final Function(BuildContext) selectAlarmTime; // Receive selectAlarmTime callback
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
 
   const ToDoListScreen({
     required this.isDarkMode,
     required this.toggleTheme,
-    required this.selectAlarmTime, // Ensure to include in constructor
+    required this.flutterLocalNotificationsPlugin,
     super.key,
   });
 
@@ -80,6 +90,7 @@ class _ToDoListScreenState extends State<ToDoListScreen> {
   final List<String> _tasks = [];
   final List<String> _completedTasks = [];
   final TextEditingController _controller = TextEditingController();
+  TimeOfDay? _selectedTime;
 
   void _addTask() {
     if (_controller.text.isNotEmpty) {
@@ -106,6 +117,92 @@ class _ToDoListScreenState extends State<ToDoListScreen> {
     });
   }
 
+  Future<void> _selectAlarmTime(BuildContext context, int index) async {
+    final TimeOfDay? pickedTime = await showTimePicker(
+      context: context,
+      initialTime: _selectedTime ?? const TimeOfDay(hour: 0, minute: 0),
+    );
+    if (pickedTime != null) {
+      setState(() {
+        _selectedTime = pickedTime;
+        _scheduleAlarm(_tasks[index]);
+      });
+    }
+  }
+
+  Future<void> _scheduleAlarm(String task) async {
+    final tz.TZDateTime now = tz.TZDateTime.now(tz.local);
+    final tz.TZDateTime scheduledDate = tz.TZDateTime(
+      tz.local,
+      now.year,
+      now.month,
+      now.day,
+      _selectedTime!.hour,
+      _selectedTime!.minute,
+    );
+
+    const AndroidNotificationDetails androidPlatformChannelSpecifics =
+        AndroidNotificationDetails(
+      'your_channel_id',
+      'your_channel_name',
+      channelDescription: 'your_channel_description',
+      importance: Importance.max,
+      priority: Priority.high,
+      showWhen: false,
+      color: Colors.blueGrey,
+      playSound: true,
+      icon: '@mipmap/ic_launcher',
+    );
+    const NotificationDetails platformChannelSpecifics =
+        NotificationDetails(android: androidPlatformChannelSpecifics);
+
+    await widget.flutterLocalNotificationsPlugin.zonedSchedule(
+      0,
+      'Task Reminder',
+      task,
+      scheduledDate,
+      platformChannelSpecifics,
+      androidAllowWhileIdle: true,
+      matchDateTimeComponents: DateTimeComponents.time,
+      uiLocalNotificationDateInterpretation: UILocalNotificationDateInterpretation.absoluteTime,
+    );
+  }
+
+  void _showTaskOptions(int index) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) => Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ListTile(
+            leading: const Icon(Icons.check_circle, color: Colors.teal),
+            title: const Text('Complete Task'),
+            onTap: () {
+              Navigator.pop(context);
+              _completeTask(index);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.delete, color: Colors.redAccent),
+            title: const Text('Delete Task'),
+            onTap: () {
+              Navigator.pop(context);
+              _deleteTask(index);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.alarm, color: Colors.blueGrey),
+            title: const Text('Set Alarm'),
+            onTap: () {
+              Navigator.pop(context);
+              _selectAlarmTime(context, index);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -116,36 +213,15 @@ class _ToDoListScreenState extends State<ToDoListScreen> {
             icon: Icon(widget.isDarkMode ? Icons.wb_sunny : Icons.nights_stay),
             onPressed: widget.toggleTheme,
           ),
-          IconButton(
-            icon: const Icon(Icons.alarm),
-            onPressed: () {
-              widget.selectAlarmTime(context); // Use selectAlarmTime from widget
-            },
-          ),
-          IconButton(
-            icon: const Icon(Icons.checklist),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => TasksToCompleteScreen(
-                    tasks: _tasks,
-                    completedTasks: _completedTasks,
-                    completeTask: _completeTask,
-                    deleteTask: _deleteTask,
-                    isDarkMode: widget.isDarkMode,
-                  ),
-                ),
-              );
-            },
-          ),
         ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Expanded(
                   child: TextField(
@@ -153,16 +229,17 @@ class _ToDoListScreenState extends State<ToDoListScreen> {
                     decoration: InputDecoration(
                       hintText: 'Enter a task',
                       hintStyle: TextStyle(
-                        color: widget.isDarkMode ? Colors.white : Colors.black,
+                        color: widget.isDarkMode ? Colors.white60 : Colors.black54,
                       ),
                       filled: true,
                       fillColor: widget.isDarkMode
-                          ? const Color.fromARGB(255, 48, 48, 48)
-                          : const Color.fromARGB(255, 230, 230, 230),
+                          ? const Color.fromARGB(255, 50, 50, 50)
+                          : const Color.fromARGB(255, 255, 255, 255),
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(12.0),
                         borderSide: BorderSide.none,
                       ),
+                      contentPadding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 14.0),
                     ),
                     style: TextStyle(
                       color: widget.isDarkMode ? Colors.white : Colors.black,
@@ -172,17 +249,151 @@ class _ToDoListScreenState extends State<ToDoListScreen> {
                 const SizedBox(width: 10),
                 ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue,
-                    padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+                    backgroundColor: Colors.blueGrey.shade800,
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 16.0),
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(12.0),
                     ),
-                    elevation: 8, // Add shadow to button
+                    elevation: 8,
+                    textStyle: const TextStyle(fontSize: 16),
                   ),
                   onPressed: _addTask,
                   child: const Icon(Icons.add, color: Colors.white),
                 ),
               ],
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Hold task to reveal options',
+              style: TextStyle(
+                fontSize: 16,
+                fontStyle: FontStyle.italic,
+                color: widget.isDarkMode ? Colors.white70 : Colors.black54,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Tasks to Complete',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.blueGrey,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _tasks.length,
+                itemBuilder: (context, index) {
+                  return Dismissible(
+                    key: Key(_tasks[index]),
+                    direction: DismissDirection.horizontal,
+                    background: Container(
+                      color: Colors.teal,
+                      alignment: Alignment.centerLeft,
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: const Icon(Icons.check_circle, color: Colors.white),
+                    ),
+                    secondaryBackground: Container(
+                      color: Colors.redAccent,
+                      alignment: Alignment.centerRight,
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: const Icon(Icons.delete, color: Colors.white),
+                    ),
+                    confirmDismiss: (direction) async {
+                      if (direction == DismissDirection.startToEnd) {
+                        // Handle complete task
+                        _completeTask(index);
+                        return false; // Prevent dismiss
+                      } else if (direction == DismissDirection.endToStart) {
+                        // Handle delete task
+                        return showDialog(
+                          context: context,
+                          builder: (context) => AlertDialog(
+                            title: const Text('Confirm'),
+                            content: const Text('Are you sure you want to delete this task?'),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop(false);
+                                },
+                                child: const Text('Cancel'),
+                              ),
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop(true);
+                                  _deleteTask(index);
+                                },
+                                child: const Text('Delete'),
+                              ),
+                            ],
+                          ),
+                        );
+                      }
+                      return false;
+                    },
+                    child: GestureDetector(
+                      onLongPress: () => _showTaskOptions(index),
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16.0),
+                        ),
+                        elevation: 6,
+                        margin: const EdgeInsets.symmetric(vertical: 6.0),
+                        child: ListTile(
+                          contentPadding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+                          title: Text(
+                            _tasks[index],
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: widget.isDarkMode ? Colors.white : Colors.black,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Completed Tasks',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.blueGrey,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _completedTasks.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16.0),
+                    ),
+                    elevation: 6,
+                    margin: const EdgeInsets.symmetric(vertical: 6.0),
+                    child: ListTile(
+                      contentPadding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+                      title: Text(
+                        _completedTasks[index],
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: widget.isDarkMode ? Colors.white : Colors.black,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  );
+                },
+              ),
             ),
           ],
         ),
